b51f0a2b470be8457c3034c1fdb021a8a46e608b
Mari <maribelsuppi97@hotmail.com>
2020-09-22 13:48:01 -0300

    login

---
+++ components/Input.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import { Input, Button } from 'react-native-elements';
+
+const InputText = ()=>{
+    return(
+            <View
+                style={styles.container}
+            >
+               <Input
+                placeholder='Usuario'
+                />
+               <Input
+                placeholder='Contraseña'
+                />
+                <Button 
+                title="Ingresar" />
+               
+            </View>
+        )
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        marginRight: 20,
+        marginLeft:20
+
+    },
+    input:{
+        margin: 25
+    }
+})
+
+
+
+export default InputText;
---
+++ screen/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import InputText from './../components/Input'
+
+const Login = ()=>{
+    return(
+        <View >
+           <InputText/>
+        </View>
+    )
+
+}
+
+
+const styles = StyleSheet.create({
+  
+})
+
+
+export default Login;
