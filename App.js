8374832c83948533090945e864d4ca217d4605ae
Mari <maribelsuppi97@hotmail.com>
2020-09-23 11:16:31 -0300

    navigation

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DrawerScreen from './screen/navigation/Drawer';
@@ -7,14 +6,9 @@ import { firebaseApp } from './firebase/firebase';
 import * as firebase from 'firebase';
 
 export default function App() {
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) =>{
-      console.log(user);
-    })
-  }, [])
 
   return (
-    <>
+    <>  
       <DrawerScreen />
     </>
   )
--- screen/navigationDrawer/Drawer.js
+++ screen/navigationDrawer/Drawer.js
@@ -4,17 +4,19 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './HomeStack';
 import PerfilStack from './PerfilStack';
-
+import Login from '../Login';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerScreen() {
   return (
+
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Login">
         <Drawer.Screen name="Home" component={HomeStack} options={{title: "Home"}} />
         <Drawer.Screen name="Perfil" component={PerfilStack} options={{title: "Perfil"}} />
       </Drawer.Navigator>
     </NavigationContainer>
+
   );
 }
